7b68cd63ac6cd5ecb1b45055bbecbe39daeee7d0
SAKaNa567 <s1240147@u-aizu.ac.jp>
2019-07-10 18:02:17 +0900

    wip

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::StoresController < ApplicationController
     @store = Store.new(store_params)
 
     if @store.save
-      render json: @store, status: :created, location: @store
+      render json: @store, status: :created
     else
       render json: @store.errors, status: :unprocessable_entity
     end
