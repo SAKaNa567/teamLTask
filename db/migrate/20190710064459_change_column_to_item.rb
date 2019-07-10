6c0ea355402d096f38363e98f069ba39fd4096a8
SAKaNa567 <s1240147@u-aizu.ac.jp>
2019-07-10 15:51:50 +0900

    db:migrate

--- db/migrate/20190710064459_change_column_to_item.rb
+++ db/migrate/20190710064459_change_column_to_item.rb
@@ -2,6 +2,6 @@ class ChangeColumnToItem < ActiveRecord::Migration[5.2]
   def change
     change_column_null :items, :title, false
     change_column_null :items, :price, false
-    chnage_column_null :items, :description, false
+    change_column_null :items, :description, false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_081753) do
+ActiveRecord::Schema.define(version: 2019_07_10_064459) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
-    t.integer "price"
-    t.text "description"
+    t.string "title", null: false
+    t.integer "price", null: false
+    t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture_file_name"
@@ -25,4 +25,22 @@ ActiveRecord::Schema.define(version: 2019_07_09_081753) do
     t.index ["title"], name: "index_items_on_title", unique: true
   end
 
+  create_table "product_stores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "store_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_product_stores_on_item_id"
+    t.index ["store_id"], name: "index_product_stores_on_store_id"
+  end
+
+  create_table "stores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_stores_on_name", unique: true
+  end
+
+  add_foreign_key "product_stores", "items"
+  add_foreign_key "product_stores", "stores"
 end
