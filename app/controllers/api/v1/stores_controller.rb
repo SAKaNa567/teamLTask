836ba531ddc2e6d8621cd27f5c7c092ed354956f
SAKaNa567 <s1240147@u-aizu.ac.jp>
2019-07-10 15:09:16 +0900

    store controller

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -46,6 +46,6 @@ class StoresController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def store_params
-      params.require(:store).permit(:name)
+      params.require(:store).permit(:name, { product_stores_attributes: [ :product_id]})
     end
 end
