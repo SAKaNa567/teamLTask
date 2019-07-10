5c9dc57a527f3688b477e45b60cc252df945a929
SAKaNa567 <s1240147@u-aizu.ac.jp>
2019-07-10 15:57:45 +0900

    [fix typo] store Model

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,5 @@
 class Store < ApplicationRecord
     has_many :items, :through => :product_stores
     has_many :product_stores, dependent: :destroy
-    accepts_nested_attributes_for :products_stores, allow_destroy: true
+    accepts_nested_attributes_for :product_stores, allow_destroy: true
 end
